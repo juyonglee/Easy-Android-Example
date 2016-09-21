aadff4ce8f57a157e87e9fba2b82e5a23fec2c0a
LeeJuYong <goal0208@naver.com>
2016-09-21 20:20:35 +0900

    First Layout

---
+++ app/src/main/java/info/zpat/goal0208/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package info.zpat.goal0208.myapplication;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView text = (TextView) findViewById(R.id.mainText);
+        text.setText("Main Menu");
+        text.setTextColor(Color.GREEN);
+    }
+}
